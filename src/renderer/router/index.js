a4d02f8a8c38cb7a230478d0e4194e91eb5e2866
Aníbal Ferreira Filho <anibalgv@gmail.com>
2020-09-16 20:53:33 -0300

    sending project base filesS

---
+++ src/renderer/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'landing-page',
+      component: require('@/components/LandingPage').default
+    },
+    {
+      path: '/import',
+      name: 'import',
+      component: require('@/pages/tickets/import').default
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
