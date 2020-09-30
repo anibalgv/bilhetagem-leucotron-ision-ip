7b8e327293ddfdc445f03ed3518064ad9d961d3f
Aníbal Ferreira Filho <anibalgv@gmail.com>
2020-09-30 17:22:29 -0300

    configurations UI

--- src/renderer/pages/configurations.vue
+++ src/renderer/pages/configurations.vue
@@ -1,81 +1,86 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="form-group col-md-2">
-        <div class="custom-control custom-switch custom-control-success mb-1">
-          <input
-            type="checkbox"
-            class="custom-control-input "
-            id="example-sw-custom1"
-            name="example-sw-custom1"
-            v-model="configuration.app_auto_import"
-          />
-          <label class="custom-control-label" for="example-sw-custom1" style="margin-top:15px; ">
-            <small>AUTO IMPORT</small>
-          </label>
+  <div style="margin-top: -0px;">
+    <div class="col-6 border" id="div-connection" style="margin-top: -5px">
+      <div class="row">
+        <div class="form-group col-md-6">
+          <div class="custom-control custom-switch custom-control-success mb-1">
+            <input
+              type="checkbox"
+              class="custom-control-input"
+              id="example-sw-custom1"
+              name="example-sw-custom1"
+              v-model="configuration.app_auto_import"
+            />
+            <label
+              class="custom-control-label"
+              for="example-sw-custom1"
+              style="margin-top: 25px"
+            >
+              <small>AUTO IMPORT</small>
+            </label>
+          </div>
         </div>
-      </div>
-      <div class="col-md-6 col-lg-6" >
-        <label for="app_auto_import" class="small" style="margin-left:20px;">
-          INTERVAL
+        <div class="form-group col-md-6">
+          <label for="app_auto_import" class="small">
+            INTERVAL <small class="text-muted">IN HOURS</small>
+          </label>
           <input
             type="number"
             min="1"
             name="app_auto_import"
             id="app_auto_impor"
             class="form-control form-control-sm"
-            :disabled = "!configuration.app_auto_import"
+            :disabled="!configuration.app_auto_import"
             v-model="configuration.app_auto_import_interval"
           />
-        </label>
-        <small class="text-muted">HOURS</small>
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group col-md-6">
+          <label for class="small"> FTP HOST ADDRESS </label>
+          <input
+            type="text"
+            name="ftp_host"
+            id="ftp_host"
+            class="form-control form-control-sm"
+            v-model="configuration.ftp_host"
+          />
+        </div>
+        <div class="form-group col-md-6">
+          <label for class="small"> FTP PORT </label>
+          <input
+            type="number"
+            name="ftp_port"
+            id="ftp_port"
+            class="form-control form-control-sm"
+            v-model="configuration.ftp_port"
+          />
+        </div>
+      </div>
+      <div class="row">
+        <div class="form-group col-md-6">
+          <label for class="small"> FTP USER </label>
+          <input
+            type="text"
+            name="ftp_user"
+            id="ftp_user"
+            class="form-control form-control-sm"
+            v-model="configuration.ftp_user"
+          />
+        </div>
+        <div class="form-group col-md-6">
+          <label for class="small"> FTP PASSWORD </label>
+          <input
+            type="text"
+            name="ftp_password"
+            id="ftp_password"
+            class="form-control form-control-sm"
+            v-model="configuration.ftp_password"
+          />
+        </div>
       </div>
     </div>
-    <div class="form-group">
-      <label for class="small">
-        FTP HOST ADDRESS
-        <input
-          type="text"
-          name="ftp_host"
-          id="ftp_host"
-          class="form-control form-control-sm"
-          v-model="configuration.ftp_host"
-        />
-      </label>
-      <label for class="small">
-        FTP PORT
-        <input
-          type="number"
-          name="ftp_port"
-          id="ftp_port"
-          class="form-control form-control-sm"
-          v-model="configuration.ftp_port"
-        />
-      </label>
-    </div>
-    <div class="form-group">
-      <label for class="small">
-        FTP USER
-        <input
-          type="text"
-          name="ftp_user"
-          id="ftp_user"
-          class="form-control form-control-sm"
-          v-model="configuration.ftp_user"
-        />
-      </label>
-      <label for class="small">
-        FTP PASSWORD
-        <input
-          type="text"
-          name="ftp_password"
-          id="ftp_password"
-          class="form-control form-control-sm"
-          v-model="configuration.ftp_password"
-        />
-      </label>
-    </div>
-    <div class="form-group">
+    <div class="form-group" style="margin-top: 10px">
       <button class="btn btn-success" v-on:click="setConfig">
         <i class="fa fa-save fa-2x"></i>
       </button>
@@ -85,9 +90,9 @@
 
 <script>
 import Configurations from "../models/configurations";
-import { alert, success, defaultModules, Stack } from '@pnotify/core';
-import '@pnotify/core/dist/PNotify.css';
-import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
+import { alert, success, defaultModules, Stack } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import * as PNotifyBootstrap4 from "@pnotify/bootstrap4";
 defaultModules.set(PNotifyBootstrap4, {});
 // import * as PNotifyDesktop from '@pnotify/desktop';
 // defaultModules.set(PNotifyDesktop, {});
@@ -106,9 +111,12 @@ export default {
     setConfig: function () {
       let saved = new Configurations().setConfiguration(this.configuration);
       if (saved) {
-        success({title:'CONFIGURATIONS', text:'SAVE SUCCESS', type: 'success'});
-        this.$router.push('imports');
-
+        success({
+          title: "CONFIGURATIONS",
+          text: "SAVE SUCCESS",
+          type: "success",
+        });
+        this.$router.push("imports");
       }
     },
   },
