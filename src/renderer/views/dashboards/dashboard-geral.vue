e9ff9c769bfdbac49b951e40d7c3ad933d70e319
Aníbal Ferreira Filho <anibalgv@gmail.com>
2020-12-10 08:36:16 -0300

    update dashboard-geral - sort e limit detail informations , implement realizadas chart

--- src/renderer/views/dashboards/dashboard-geral.vue
+++ src/renderer/views/dashboards/dashboard-geral.vue
@@ -3,7 +3,7 @@
     <div class="row" style="z-index: 4; margin-top: -20px;">
         <DateBarComponent @onSelect="getSelectedDate" />
     </div>
-    <div class="row"  style="z-index: 1; margin-top: -60;">
+    <div class="row"  style="z-index: 1; margin-top: -60; backgroud-color: #ffffff">
       <div class="col-6">
         <ChartPie id="generalChart" label="GENERAL INFOS" :labels="generalChart.labels" :data="generalChart.data" :options='generalChart.options' />
       </div>
@@ -24,10 +24,10 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-5">
-        <ChartHorizontalBar id="received-chart" label="RECEIVED INFOS" :labels="receivedChart.labels" :data="receivedChart.data" :options='generalChart.options' />
+      <div class="col-6">
+        <ChartHorizontalBar id="received-chart" label="RECEBIDAS" :labels="receivedChart.labels" :data="receivedChart.data" :options='generalChart.options' />
       </div>
-      <div class="col-4"></div>
+      <div class="col-3"></div>
       <div class="col-3" style="margin-top: 0px;" v-show="receivedChart.labels[0]">
         <table class="table table-sm small table-striped">
           <thead>
@@ -37,6 +37,26 @@
           </thead>
           <tbody>
             <tr v-for="info in receivedChart.info" :key="info.responsavel">
+              <td>{{info.responsavel}}</td> <td align="right">{{ info.percent}} %</td>  <td align="right"> {{info.count}}</td> 
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6">
+        <ChartHorizontalBar id="realizadas-chart" label="ORIGINADAS" :labels="realizadasChart.labels" :data="realizadasChart.data" :options='generalChart.options' />
+      </div>
+      <div class="col-3"></div>
+      <div class="col-3" style="margin-top: 0px;" v-show="realizadasChart.labels[0]">
+        <table class="table table-sm small table-striped">
+          <thead>
+           <th>RAMAL </th>
+           <th>PERCENT</th>
+           <th>TOTAL</th>
+          </thead>
+          <tbody>
+            <tr v-for="info in realizadasChart.info" :key="info.responsavel">
               <td>{{info.responsavel}}</td> <td align="right">{{ info.percent}} %</td>  <td align="right"> {{info.count}}</td> 
             </tr>
           </tbody>
@@ -81,6 +101,12 @@ export default {
         info: [],
         percent: [],
       },
+      realizadasChart: {
+        labels : [],
+        data: [],
+        info: [],
+        percent: [],
+      },
       selectedDate : null,
       tickets : [],
     };
@@ -93,7 +119,8 @@ export default {
       this.selectedDate = _response.date;
       this.tickets = new Tickets().getByYearMonthDay( this.selectedDate.year, this.selectedDate.month, this.selectedDate.day );
       this.renderGeneralChart();
-      this.renderOriginatedChart();
+      this.RenderRecebidasChart();
+      this.RenderOriginadasChart();
     },
     renderGeneralChart(){
       console.log('[RENDERGENERALCHART]');
@@ -110,13 +137,14 @@ export default {
           percent: ((100 / this.tickets.length) * value.length ).toFixed(2)  
         });
       });
+      
       this.generalChart.labels = labels;
       this.generalChart.data = values;
-      this.generalChart.info = info;
+      this.generalChart.info = new Reports().SortByCountInverse(info);
       console.log('[RENDERGENERALCHART]', this.generalChart);
     },
-    renderOriginatedChart(){
-      console.log('[RENDERORIGINATEDCHART]');
+    RenderRecebidasChart(){
+      console.log('[RENDERRECEBIDASCHART]');
       const filteredTickets = this.tickets.filter((item)=>{
         return item.tipo == "RECEBIDA";
       });
@@ -138,10 +166,32 @@ export default {
       });
       this.receivedChart.labels = labels;
       this.receivedChart.data = data;
-      this.receivedChart.info = info;
-      console.log('filteredTickets :', filteredTickets);
-      console.log('tickets :', tickets);
-      console.log('receivedChart :', this.receivedChart);
+      this.receivedChart.info = new Reports().SortByCountInverse(info).slice(0, 10);
+    },
+    RenderOriginadasChart(){
+      console.log('[RENDERORIGINADASCHART]');
+      const filteredTickets = this.tickets.filter((item)=>{
+        return item.tipo == "ORIGINADA";
+      });
+      let tickets = filteredTickets.reduce((prev, current, index)=>{
+        prev[current.responsavel] = prev[current.responsavel] ? (prev[current.responsavel] + 1) : 1 ;
+        return prev;
+      },{});
+      let labels = [];
+      let data = [];
+      let info = [];
+      Object.entries(tickets).forEach(([key, value])=>{
+        labels.push(key);
+        data.push(value);
+        info.push({
+          responsavel: key, 
+          count : value,
+          percent: (( 100 / filteredTickets.length) * value).toFixed(2),
+        });
+      });
+      this.realizadasChart.labels = labels;
+      this.realizadasChart.data = data;
+      this.realizadasChart.info = info;
     },
   },  //end methods
  
