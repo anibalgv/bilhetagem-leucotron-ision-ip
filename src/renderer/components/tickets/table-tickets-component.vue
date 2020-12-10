759bb3056905f73a45f4327bc8588d9960e4bd30
Aníbal Ferreira Filho <anibalgv@gmail.com>
2020-12-10 08:31:12 -0300

    implement sorte by column table-tickets-component

--- src/renderer/components/tickets/table-tickets-component.vue
+++ src/renderer/components/tickets/table-tickets-component.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="row col-12">
+    <!-- <input type="text" name="" id="filter-term"> -->
     <table class="table table-sm table-striped small table-hover" style="margin-top: 10px" >
       <thead>
-        <th>DATA</th>
-        <th>HORA</th>
-        <th>RAMAL</th>
-        <th>NOME USUARIO</th>
+        <th><a href="#" @click="sort($event, 'id')"> DATA  </a></th>
+        <th><a href="#" @click="sort($event, 'hora_inicio')"> HORA  </a></th>
+        <th><a href="#" @click="sort($event, 'responsavel')"> RAMAL </a></th>
+        <th><a href="#" @click="sort($event, 'nome_usuario')"> NOME USUARIOAL </a></th>
         <th></th>
-        <th>TIPO</th>
-        <th>Nº EXTERNO</th>
-        <th>ATRIBUTO</th>
+        <th><a href="#" @click="sort($event, 'tipo')"> TIPO </a></th>
+        <th><a href="#" @click="sort($event, 'numero_externo')"> Nº EXTERNO </a></th>
+        <th><a href="#" @click="sort($event, 'atributo')"> ATRIBUTO </a></th>
         <th>ATENDIMENTO</th>
         <th>DURACAO</th>
       </thead>
       <tBody>
-        <tr v-for="ticket in tickets" :key="ticket.id">
+        <tr v-for="ticket in orderedTickets" :key="ticket.id" >
           <td>{{ ticket.data_inicio }}</td>
           <td align="center">{{ ticket.hora_inicio }}</td>
           <td align="center">{{ ticket.responsavel }}</td>
@@ -32,9 +33,29 @@
 </template>
 
 <script>
+var _ = require('lodash');
 export default {
   name: 'TableTickets',
   props: ["tickets"],
+  data: function(){
+    return {
+      sortProperty: 'id',
+      // sortDirection: 'desc',
+      filterTerm: '',
+    }
+  },
+  computed : {
+    orderedTickets: function(){
+      return _.sortBy(this.tickets, this.sortProperty );
+    },
+  },
+  methods: {
+    sort: function(_event, _sortProperty){
+      _event.preventDefault();
+      this.sortProperty = _sortProperty;
+      // this.sortDirection = this.sortDirection == 'asc' ? 'desc' : 'asc'
+    },
+  },
 };
 </script>
 
